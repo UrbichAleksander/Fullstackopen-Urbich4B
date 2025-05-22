5f7057f6a592634e3cfce27516d6b56e188f542a
UrbichAleksander <olekurbich1@wp.pl>
2025-05-22 10:10:18 +0200

    zmiany

--- FSO_Part3/PhonebookBack/index.js
+++ FSO_Part3/PhonebookBack/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-
+app.use(morgan('tiny'))
 let persons =  [
         { 
           "id": "1",
@@ -51,7 +52,6 @@ app.get('/api/persons', (request, response) => {
   app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
     persons = persons.filter(person => person.id !== id)
-  
     response.status(204).end()
   })
 
@@ -63,22 +63,27 @@ app.get('/api/persons', (request, response) => {
   app.post('/api/persons', (request, response) => {
     const body = request.body
   
-    if (!body.content) {
+    if (!body.name || !body.number) {
       return response.status(400).json({ 
-        error: 'content missing' 
+        error: 'Name and number cannot be empty' 
       })
     }
-  
+    
+    if (persons.some(person => person.name === body.name)) {
+      return response.status(400).json({
+        error: 'name must be unique'
+      })
+    }
+
     const person = {
-      content: body.content,
-      important: body.important || false,
+      name: body.name,
+      number: body.number,
       id: generateId(),
     }
   
     persons = persons.concat(person)
   
     response.json(person)
-    
   })
 
 
